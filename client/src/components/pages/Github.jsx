37481f39da7e49b68c00dc6e583aa8d59d4fd88f
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-01-18 14:05:15 -0500

    moved Github stuff to /Github

--- client/src/components/pages/Github.jsx
+++ client/src/components/pages/Github.jsx
@@ -1,9 +1,12 @@
-const Github = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import api from "../../hooks/api"
+import ErrorIndicator from "../common/ErrorIndicator"
 
-export default Github;
+const Github = () => {
+  const { activity, error } = api.useGithubActivity("roccomaniscalco", 2020)
+
+  if (error) return <ErrorIndicator />
+  if (activity) return <div>{JSON.stringify(activity, null, 20)}</div>
+  return <div>loading...</div>
+}
+
+export default Github
--- client/src/components/pages/Index.jsx
+++ client/src/components/pages/Index.jsx
@@ -1,12 +1,5 @@
-import api from "../../hooks/api"
-import ErrorIndicator from "../common/ErrorIndicator"
-
 const Index = () => {
-  const { activity, error } = api.useGithubActivity("roccomaniscalco", 2020)
-
-  if (error) return <ErrorIndicator />
-  if (activity) return <div>{JSON.stringify(activity)}</div>
-  return <div>loading...</div>
+  return <div>Hello Grassbox!</div>
 }
 
 export default Index
