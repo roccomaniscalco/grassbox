e9e3ad3449d5a4f4666b376f6d91b5982b5cbdc6
Rocco Maniscalco <roccomaniscalco2001@gmail.com>
2022-01-29 06:55:47 -0500

    styled and added dynamic year options to GithubPreferences

--- client/src/components/widgets/github/GithubPreferences.jsx
+++ client/src/components/widgets/github/GithubPreferences.jsx
@@ -7,33 +7,42 @@ import {
 } from "@mui/material"
 import { useGithubContext } from "../../contexts/GithubContext"
 
+const getLastYears = (yearCount) => {
+  const lastYears = ["current"]
+  const currentDate = new Date()
+  for (let i = 0; i < yearCount; i++) {
+    lastYears.push(currentDate.getFullYear() - i)
+  }
+  return lastYears
+}
+
 const GithubPreferences = () => {
   const { year, setYear } = useGithubContext()
+  const lastYears = getLastYears(3)
+
   const handleChange = (_, year) => year && setYear(year)
 
   return (
     <Paper>
-      <Stack p={2} gap={2} width={180}>
-        <Typography variant="caption">Github Preferences</Typography>
+      <Stack p={2} gap={2} width={170}>
+        <Typography variant="caption" color="textSecondary">
+          Github Preferences
+        </Typography>
         <ToggleButtonGroup
           orientation="vertical"
           value={year}
           exclusive
           onChange={handleChange}
           size="small"
+          color="success"
         >
-          <ToggleButton value="current" aria-label="current">
-            <Typography variant="body2">current</Typography>
-          </ToggleButton>
-          <ToggleButton value="2022" aria-label="2022">
-            <Typography variant="body2">2022</Typography>
-          </ToggleButton>
-          <ToggleButton value="2021" aria-label="2021">
-            <Typography variant="body">2021</Typography>
-          </ToggleButton>
-          <ToggleButton value="2020" aria-label="2020">
-            <Typography variant="body">2020</Typography>
-          </ToggleButton>
+          {lastYears.map((year) => (
+            <ToggleButton value={year} aria-label={year} key={year}>
+              <Typography variant="body2" sx={{ textTransform: "capitalize" }}>
+                {year}
+              </Typography>
+            </ToggleButton>
+          ))}
         </ToggleButtonGroup>
       </Stack>
     </Paper>
